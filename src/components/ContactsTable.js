300fb3a87cc1362ac600671deb468df05ef2b0f7
Kartik-Jagdale <kartikpucsd2022@gmail.com>
2024-11-17 11:22:05 +0530

    updated readme file

--- src/components/ContactsTable.js
+++ src/components/ContactsTable.js
@@ -1,42 +1,3 @@
-/*import React from "react";
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Button } from "@mui/material";
-
-const ContactsTable = ({ contacts, onEdit, onDelete }) => {
-  return (
-    <TableContainer>
-      <Table>
-        <TableHead>
-          <TableRow>
-            {["First Name", "Last Name", "Email", "Phone Number", "Company", "Job Title", "Actions"].map((col) => (
-              <TableCell key={col}>{col}</TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {contacts.map((contact) => (
-            <TableRow key={contact.id}>
-              {["firstName", "lastName", "email", "phoneNumber", "company", "jobTitle"].map((field) => (
-                <TableCell key={field}>{contact[field]}</TableCell>
-              ))}
-              <TableCell>
-                <Button color="primary" onClick={() => onEdit(contact.id)}>
-                  Edit
-                </Button>
-                <Button color="secondary" onClick={() => onDelete(contact.id)}>
-                  Delete
-                </Button>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
-};
-
-export default ContactsTable;
-*/
-
 import React from "react";
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Button } from "@mui/material";
 
@@ -74,4 +35,3 @@ const ContactsTable = ({ contacts, onEdit, onDelete }) => {
 };
 
 export default ContactsTable;
-
